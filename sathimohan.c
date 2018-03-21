a2f1eaabe34cc0984c91609d2d107cd872a6ea75
sakthigowri01 <35915299+sakthigowri01@users.noreply.github.com>
2018-03-21 15:49:52 +0530

    Update sathimohan.c

--- sathimohan.c
+++ sathimohan.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include<math.h>
 int main(void) {
-	int a,b,c,d;
-	scanf("%d %d %d",&a,&b,&c);
+	int a,b,c,d,e;
+	scanf("%d %d %d",&a,&b,&c,&d);
 	d=(a*b)/c;
-	printf("%d",d);
-	
 	
 	
 	
